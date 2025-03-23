7b89e58eb4f090336e894ac9cf6dcac8aa35f1d5
Triysle <triyslebrin@gmail.com>
2025-03-22 21:58:24 -0700

    Revert "0.0.4"
    This reverts commit 65f9cbd1b5e791359a3f904898c524abdd7d586e.

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,10 +6,8 @@ signal health_changed(health_value)
 @onready var anim_player = $AnimationPlayer
 @onready var muzzle_flash = $Camera3D/Pistol/MuzzleFlash
 @onready var raycast = $Camera3D/RayCast3D
-@onready var state_machine = $StateMachine
 
 var health = 3
-var states = {}
 
 const SPEED = 7.0
 const JUMP_VELOCITY = 10
@@ -25,25 +23,52 @@ func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	camera.current = true
 	
-	# Initialize states dict for easy access in state classes
-	states.idle = $StateMachine/IdleState
-	states.move = $StateMachine/MoveState
-	states.jump = $StateMachine/JumpState
-	states.fall = $StateMachine/FallState
-	states.shoot = $StateMachine/ShootState
-	
-	# Set the player reference in the state machine
-	state_machine.player = self
-	
 func _unhandled_input(event):
 	if not is_multiplayer_authority(): return
 	if event is InputEventMouseMotion:
 		rotate_y(-event.relative.x * .005)
 		camera.rotate_x(-event.relative.y * .005)
 		camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
+		
+	if Input.is_action_just_pressed("shoot") and anim_player.current_animation != "shoot":
+		play_shoot_effects.rpc()
+		if raycast.is_colliding():
+			var hit_player = raycast.get_collider()
+			if hit_player is CharacterBody3D:
+				var hit_position = raycast.get_collision_point()
+				request_hit_validation.rpc_id(1, hit_player.name, hit_position)
+	
 
-func calculate_gravity() -> Vector3:
-	return Vector3.DOWN * gravity
+func _physics_process(delta):
+	if not is_multiplayer_authority(): return
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	if anim_player.current_animation == "shoot":
+		pass
+	elif input_dir != Vector2.ZERO and is_on_floor():
+		anim_player.play("move")
+	else:
+		anim_player.play("idle")
+
+
+	move_and_slide()
 
 @rpc("call_local")
 func play_shoot_effects():
